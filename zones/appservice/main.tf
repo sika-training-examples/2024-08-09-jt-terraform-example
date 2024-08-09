495c4b1c1374af6cb01bd26cce2a04c0abb223e4
Ondrej Sika <ondrej@ondrejsika.com>
2024-08-09 15:55:49 +0200

    feat(zones/appservice): Use hello_jt module & create dev and test environments

--- zones/appservice/main.tf
+++ zones/appservice/main.tf
@@ -1,32 +1,11 @@
+module "hello_jt_dev" {
+  source = "../../modules/hello_jt"
 
-resource "azurerm_resource_group" "rg" {
-  name     = "jt-appservice"
-  location = "westeurope"
+  env = "dev"
 }
 
-resource "azurerm_service_plan" "main" {
-  name                = "jt"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-}
+module "hello_jt_test" {
+  source = "../../modules/hello_jt"
 
-
-module "hello-jt" {
-  source = "../../modules/app"
-
-  config = {
-    name                = "jt-demo-terraform"
-    location            = azurerm_service_plan.main.location
-    resource_group_name = azurerm_service_plan.main.resource_group_name
-    service_plan_id     = azurerm_service_plan.main.id
-    docker_image_name   = "sikalabs/hello-world-server"
-    docker_registry_url = "https://index.docker.io"
-    worker_count        = 2
-    env = {
-      PORT = "80"
-      TEXT = "Hello J&T"
-    }
-  }
+  env = "test"
 }
