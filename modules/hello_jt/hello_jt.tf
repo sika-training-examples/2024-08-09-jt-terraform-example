36def507c06ea34071c0a7cea8f1a35cc5a68a00
Ondrej Sika <ondrej@ondrejsika.com>
2024-08-09 15:55:16 +0200

    feat(modules/hello_jt): Create module for Hello JT application

---
+++ modules/hello_jt/hello_jt.tf
@@ -0,0 +1,48 @@
+variable "env" {
+  type        = string
+  description = "Application environment (prod, stage, test, or dev)"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure Location"
+  default     = "westeurope"
+}
+
+variable "worker_count" {
+  type        = number
+  description = "Number of workers"
+  default     = 1
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "hello-jt-${var.env}"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "this" {
+  name                = "hello-jt-${var.env}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+
+module "app" {
+  source = "../app"
+
+  config = {
+    name                = "hello-jt-${var.env}"
+    location            = azurerm_service_plan.this.location
+    resource_group_name = azurerm_service_plan.this.resource_group_name
+    service_plan_id     = azurerm_service_plan.this.id
+    docker_image_name   = "sikalabs/hello-world-server"
+    docker_registry_url = "https://index.docker.io"
+    worker_count        = var.worker_count
+    env = {
+      PORT = "80"
+      TEXT = "Hello J&T (${var.env})"
+    }
+  }
+}
