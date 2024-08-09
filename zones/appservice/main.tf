7bc23180459b2fd2ceb46867f78e48a119d4edd3
Ondrej Sika <ondrej@ondrejsika.com>
2024-08-09 16:35:17 +0200

    feat(zones/appservice): Create example of hello_jt_v2 module

--- zones/appservice/main.tf
+++ zones/appservice/main.tf
@@ -9,3 +9,29 @@ module "hello_jt_test" {
 
   env = "test"
 }
+
+///////////////////////////
+
+module "rgs" {
+  source = "../../modules/rg"
+
+  for_each = toset(["stage", "prod"])
+
+  name = each.key
+}
+
+module "hello_jt_stage" {
+  source = "../../modules/hello_jt_v2"
+
+  env                 = "stage"
+  resource_group_name = module.rgs["stage"].name
+  location            = module.rgs["stage"].location
+}
+
+module "hello_jt_prod" {
+  source = "../../modules/hello_jt_v2"
+
+  env                 = "prod"
+  resource_group_name = module.rgs["prod"].name
+  location            = module.rgs["prod"].location
+}
