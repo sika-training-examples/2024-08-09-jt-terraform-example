7b861a5bb2cb83249d2bc8c2417d7d4b3b4cf421
Ondrej Sika <ondrej@ondrejsika.com>
2024-08-12 12:24:41 +0200

    feat(modules/app): Add identity for key vault

--- modules/app/app.tf
+++ modules/app/app.tf
@@ -12,6 +12,12 @@ variable "config" {
   })
 }
 
+resource "azurerm_user_assigned_identity" "this" {
+  name                = "${var.config.name}-appservice"
+  location            = var.config.location
+  resource_group_name = var.config.resource_group_name
+}
+
 resource "azurerm_linux_web_app" "this" {
   app_settings = merge({
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
@@ -31,4 +37,19 @@ resource "azurerm_linux_web_app" "this" {
   ftp_publish_basic_authentication_enabled       = false
   https_only                                     = var.config.https_only
   webdeploy_publish_basic_authentication_enabled = false
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.this.id
+    ]
+  }
+  key_vault_reference_identity_id = azurerm_user_assigned_identity.this.id
+}
+
+output "principal_id" {
+  value = azurerm_user_assigned_identity.this.principal_id
+}
+
+output "tenant_id" {
+  value = azurerm_user_assigned_identity.this.tenant_id
 }
