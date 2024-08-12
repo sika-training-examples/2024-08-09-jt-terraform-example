6fdaf7635cad1c1283735faee0c650f1d64183c7
Ondrej Sika <ondrej@ondrejsika.com>
2024-08-12 22:13:39 +0200

    feat(zones/aks): Add AKS example

---
+++ zones/aks/main.tf
@@ -0,0 +1,91 @@
+
+resource "azurerm_resource_group" "example" {
+  name     = "jt-aks"
+  location = "swedencentral"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "jt-aks"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "jtaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
+
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "rg" {
+  type        = string
+  default     = "rg-avd-compute"
+  description = "Name of the Resource group in which to deploy session host"
+}
+
+variable "rdsh_count" {
+  description = "Number of AVD machines to deploy"
+  default     = 2
+}
+
+variable "prefix" {
+  type        = string
+  default     = "avdtf"
+  description = "Prefix of the name of the AVD machine(s)"
+}
+
+variable "domain_name" {
+  type        = string
+  default     = "infra.local"
+  description = "Name of the domain to join"
+}
+
+variable "domain_user_upn" {
+  type        = string
+  default     = "domainjoineruser" # do not include domain name as this is appended
+  description = "Username for domain join (do not include domain name as this is appended)"
+}
+
+variable "domain_password" {
+  type        = string
+  default     = "ChangeMe123!"
+  description = "Password of the user to authenticate with the domain"
+  sensitive   = true
+}
+
+variable "vm_size" {
+  description = "Size of the machine to deploy"
+  default     = "Standard_DS2_v2"
+}
+
+variable "ou_path" {
+  default = ""
+}
+
+variable "local_admin_username" {
+  type        = string
+  default     = "localadm"
+  description = "local admin username"
+}
+
+variable "local_admin_password" {
+  type        = string
+  default     = "ChangeMe123!"
+  description = "local admin password"
+  sensitive   = true
+}
